b9d9c321131ee3471e9db82bb690879293958ee3
waylandsingh <wmsingh@ucdavis.edu>
2021-06-24 11:51:08 -0700

    partial solution, no priority operations

--- logic.js
+++ logic.js
@@ -26,34 +26,64 @@ function operate(func, a, b) {
     return operations[func](a,b)
 }
 
-numberEventListeners = number => {
-    number.addEventListener('click', (e) => {
-        workingOperator += number.id
-        console.log(workingOperator)
-        results.textContent = workingOperator
-
-    })
-}
-
 // GLOBAL variables for state   
 // arrays for operator/ands
-let operators = [];
-let operands = [];
-let workingOperator = '';
+let workingOperator = null;
+let firstOperand = '';
+let previousOperand = null
+let workingOperand = '';
 // immediately evaluate on the next operator input
 let evalImmediately = false;
 
 const results = document.querySelector('#results')
 
 // numbers append and change display
-const numbers = document.querySelectorAll('.number');
-numbers.forEach(numberEventListeners)
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if (previousOperand) {
+            workingOperand += number.id
+            results.textContent = workingOperand
+        } else {
+            firstOperand += number.id
+            results.textContent = firstOperand
+        }
+
+ 
+    })
+})
+
+
 
 // operators "flash" the working set the 'immediate eval' flag
-// need to get and store operands and operators
+function operatorEvent(now=evalImmediately) {
+    console.log(now)
+    return null
+}
+// button is pressed, live evaluations instead
+const operators = document.querySelectorAll('.operator')
+operators.forEach(operator => {
+    operator.addEventListener('click', (e)=>{
+        if (firstOperand) {
+            console.log('first operand')
+            previousOperand = parseInt(firstOperand)
+            firstOperand = false
+            workingOperator = operator.id
 
-    // button is pressed
-        // if an operator and nothing in working operand, nothing
+        } else if (previousOperand) {
+            previousOperand = operate(workingOperator, previousOperand, parseInt(workingOperand))
+            console.log(previousOperand)
+            workingOperator = operator.id
+        } else {
+            console.log('incorrect')
+        }
+
+        console.log(previousOperand, workingOperator)
+    })
+})
+// if an operator and nothing in working operand, nothing
         // if operand, append to 'working operand'
         // if operator and something in working, append both respectively
     // respect order of operations during input (OR, do stacks?)
+
+// event handler attachment of operator to each operator div
